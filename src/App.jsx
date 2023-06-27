74fcf1146ab73f215c234470438ae95b81fd9707
Augasty <sayaksengupta140@gmail.com>
2023-06-28 01:09:12 +0530

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,76 @@
 
 import { useState } from 'react'
 import './App.css'
+import usePasswordGenerator from './hooks/usePasswordGenerator'
 
 function App() {
 
-  const checkboxData = [
-    {title:'Include Uppercase', state: false},
-    {title:'Include Lowercase', state: false},
-    {title:'Include Number', state: false},
-    {title:'Include Symbols', state: false}
-  ]
+  const [length, setLength] = useState(6)
+  const [checkboxData, setCheckboxData] = useState(
+    [{ title: 'Include Uppercase', state: false },
+    { title: 'Include Lowercase', state: false },
+    { title: 'Include Numbers', state: false },
+    { title: 'Include Symbols', state: false }])
 
-  const [length,setLength] = useState(6)
+  function handleCheckboxData(idx) {
+    const updatedData = [...checkboxData]
+    updatedData[idx].state = !updatedData[idx].state
+    setCheckboxData(updatedData)
+  }
+
+
+  const { password, errorMessage, generatePassword } = usePasswordGenerator()
+
+
+  const handleCopy=()=>{
+    navigator.clipboard.writeText(password)
+  }
+
+  const handleLength = (e) =>{
+    setLength(e.target.value)
+  }
+
+
+
+
+  
+
+
+
+
   return (
     <div className='container'>
       {/* password text and copy button */}
-    <div className='header'>
-      <div className='title'>ehD3#w@</div>
-      <button className='copyBtn' onClick={()=>{}}>Copy</button>
-    </div>
+      {password && <div className='header'>
+        <div className='title'>{password}</div>
+        <button className='copyBtn' onClick={() => handleCopy()}>Copy</button>
+      </div>}
 
       {/* character length slider */}
       <div className='charLength'>
         <span>
           <label>Character length: </label>
-          <label>{length}</label>
+          <label>{length} </label>
         </span>
-        <input type='range' min='4' max='20' value={length} onChange={(e)=>setLength(e.target.value)}/>
+        <input type='range' min='4' max='20' value={length} onChange={()=>handleLength} />
       </div>
       {/* checkboxes */}
       <div className='checkboxes'>
-        {checkboxData.map((cb,idx)=>(
+        {checkboxData.map((cb, idx) => (
           <div key={idx}>
-            <input type='checkbox' checked ={cb.state}></input>
+            <input type='checkbox' checked={cb.state} onChange={() => handleCheckboxData(idx)}></input>
             <div>{cb.title}</div>
           </div>
         ))}
       </div>
+
+
+      {/* error */}
+      
+    {errorMessage && <div className='errorMessage'>{errorMessage}</div>}
+
       {/* generate button */}
+      <button className='generateBtn' onClick={() => generatePassword(checkboxData,length)}>Generate</button>
     </div>
   )
 }
---
+++ src/hooks/usePasswordGenerator.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const usePasswordGenerator = () => {
+
+    const [password, setPassword] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
+
+
+
+    const generatePassword = (checkboxData, length) => {
+        let charset = ''
+        let generatedPassword = ''
+
+        const selectedOption = checkboxData.filter(cb => cb.state)
+
+        if (selectedOption.length === 0) {
+            setErrorMessage('select at least one option')
+            setPassword('')
+            return
+        }
+
+        selectedOption.forEach(option => {
+            switch (option.title) {
+                case 'Include Uppercase':
+                    charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                    break;
+                case 'Include Lowercase':
+                    charset += 'abcdefghijklmnopqrstuvwxyz'
+                    break
+                case 'Inclue Numbers':
+                    charset += "0123456789"
+                    break;
+                case 'Include Symbols':
+                    charset += "!@#$%^&*()-_=+[]{};:,.<>/?|`~"
+                    break
+                default:
+                    break;
+            }
+        });
+        for (let index = 0; index < length; index++) {
+            const randomIdx = Math.floor(Math.random() * charset.length)
+            generatedPassword += charset[randomIdx]
+        }
+        setPassword(generatedPassword)
+        setErrorMessage('')
+    }
+
+    return { password, errorMessage, generatePassword }
+}
+
+export default usePasswordGenerator
